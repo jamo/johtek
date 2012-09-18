8e3a9c9603eaac2b62ecc86fdcd4359467ae3f26
Jarmo Isotalo <jamo@isotalo.fi>
2012-09-18 18:10:56 +0300

    aikataulut hakusessa

--- viikko2/a_star.rb
+++ viikko2/a_star.rb
@@ -26,8 +26,13 @@ end
 
 ##
 #Hakutila
+#Aika_maaliin on heuristinen arvio, ettå paljonko matkaa on vähintään(minuuteissa)
+#
+#Odotusaika, paljonko pysakilla pitää odotella
+# valimatkan_aika kuljetun välin kesto
+# odotusaika pysakilla=parent? ennen matkaa
 class Node
-  attr_accessor :pysakki, :matka, :parent, :linja, :aika_maaliin, :kulunut_aika
+  attr_accessor :pysakki, :matka, :parent, :linja, :aika_maaliin, :valimatkan_aika, :odotusaika, :aika_yhteensa
 
   def initialize pysakki, matka, parent, linja, aika_maaliin
     @pysakki = pysakki
@@ -57,7 +62,8 @@ class Node
   def to_s
     lopputulos = "Koodi: #{@pysakki.koodi} "
     lopputulos << "Linja: #{@linja.koodi}" if @linja
-    lopputulos << " Aikaa kulunut: #{@kulunut_aika} Matka:#{@matka}"
+    lopputulos << " Aikaa kulunut: #{@valimatkan_aika} Matka:#{@matka}"
+    lopputulos << " odotusaika: #{@odotusaika}"
     lopputulos
   end
 
@@ -66,13 +72,16 @@ end
 
 class AStar
 
-  attr_accessor :json_verkko, :json_linjat, :pysakki_array, :pysakit, :linjat_array, :linjat, :visited, :stack
+  attr_accessor :json_verkko, :json_linjat, :pysakki_array, :pysakit, :linjat_array, :linjat, :visited, :stack, :x, :y, :jonossa_olleet, :jx, :jy
 
   def initialize
     @pysakki_array, @pysakit, @linjat_array = [], {}, []
     @visited = []
     @stack = Stack.new
     @linjat = Hash.new
+    @jonossa_olleet = []
+    @jx = []
+    @jx = []
 
     read_json
   end
@@ -97,6 +106,7 @@ class AStar
     end
   end
 
+  #Priorityqueuen takia arvot negatiivisina :)
   def heur curr, goal
     -((curr.x- @pysakit[goal].x).abs + (curr.y - @pysakit[goal].y).abs)/526
   end
@@ -107,13 +117,19 @@ class AStar
     linja.psAjat[toka_indeksi]-linja.psAjat[eka_indeksi]
   end
 
+  def odotusaika pysakki_nyt, linja
+     4# binding.pry
+  end
 
   def haku alku="1250429", loppu="1121480", aika=0
     puts "alku #{alku} --- loppu #{loppu}"
     queue = Containers::PriorityQueue.new
     alku_node = Node.new @pysakit[alku], 0, nil, nil, (heur @pysakit[alku], loppu)
-    alku_node.kulunut_aika=0
+    alku_node.valimatkan_aika=0
+    alku_node.aika_yhteensa=0
+    alku_node.odotusaika=0
     queue.push alku_node, alku_node.aika_maaliin
+    @jonossa_olleet.push alku_node
 
 
     while !queue.empty?
@@ -123,7 +139,10 @@ class AStar
         pysakki_nyt.naapurit.each do |naapuri|
           naapuri_pysakki = @pysakit[naapuri[0]]
           naapuri_node = Node.new naapuri_pysakki, pysakki_nyt.matka+1, pysakki_nyt, @linjat[naapuri[1][0]], heur(naapuri_pysakki, loppu)
-          naapuri_node.kulunut_aika=pysakki_nyt.kulunut_aika+aika(pysakki_nyt, naapuri, @linjat[naapuri[1][0]])
+
+          naapuri_node.odotusaika=odotusaika(pysakki_nyt, @linjat[naapuri[1][0]])
+          binding.pry
+          naapuri_node.aika_yhteensa=pysakki_nyt.aika_yhteensa+pysakki_nyt.odotusaika+pysakki_nyt.valimatkan_aika
           #                       pysakki,          matka,               parent
           if naapuri_node.koodi == loppu
             return naapuri_node
@@ -131,6 +150,7 @@ class AStar
           unless @visited.include? naapuri_node.koodi
             puts "arvostus: #{naapuri_node.aika_maaliin} nyt: #{naapuri_pysakki.koodi}, loppu: #{loppu}"
             queue.push naapuri_node, naapuri_node.aika_maaliin
+            @jonossa_olleet.push naapuri_node
             #binding.pry
           end
         end
@@ -139,6 +159,8 @@ class AStar
     end
   end
 
+  #
+  #Printtaillaan koko reitti
   def hae_reitti alku, loppu, aika
     tulos = haku alku, loppu, aika
     puts "Reitti haettu. Nyt tulostukseen"
@@ -160,11 +182,31 @@ class AStar
       x_koord<< p.pysakki.x
       y_koord<< p.pysakki.y
     end
+    puts "\nLopullinen reitti:"
     @x = "x <- c(" + x_koord.join(', ') + ")"
+    puts @x
     @y = "y <- c(" + y_koord.join(', ') + ")"
+    puts @y
+    puts
+
+    jykoord = []
+    jxkoord=[]
+    @jonossa_olleet.each do |j|
+
+      jxkoord << j.pysakki.x
+      jykoord << j.pysakki.y
+    end
+    @jx =   "x <- c(" + jxkoord.join(', ') +")"
+    @jy =   "x <- c(" + jykoord.join(', ') +")"
+
+    puts
+    puts "etsityt pysakit"
+    puts @jx
+    puts @jy
   end
 
 
 end
 
-AStar.new.hae_reitti "1250429", "1121480", 0
+#AStar.new.hae_reitti "1250429", "1121480", 0
+AStar.new.hae_reitti "1230407", "1203410", 0
