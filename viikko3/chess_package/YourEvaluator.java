68b0a9e779815448046921b7cb41e88b3049c4bb
Jarmo Isotalo <jamo@isotalo.fi>
2012-09-24 23:55:42 +0300

    tmp2

--- viikko3/chess_package/YourEvaluator.java
+++ viikko3/chess_package/YourEvaluator.java
@@ -23,86 +23,114 @@
 //	}
 //}
 public class YourEvaluator extends Evaluator {
-    
-    public YourEvaluator(){
-        Main.oe = new MyNoobEvaluator();
-    }
 
+    public YourEvaluator() {
+        //Main.ce.
+//        System.out.println(Main.ce);
+//        System.out.println(Main.oe);
+//        System.out.println(Main.ye);
+        //Main.ye = new MyNoobEvaluator();
+        //Main.ce = new MyNoobEvaluator();
+    }
     static double[][] blackBoostTable = {{0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.15D, 0.2D, 0.2D, 0.15D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.2D, 0.3D, 0.3D, 0.2D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.15D, 0.1D, 0.1D, 0.15D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}};
     static double[][] whiteBoostTable = {{0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.15D, 0.1D, 0.1D, 0.15D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.2D, 0.3D, 0.3D, 0.2D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.15D, 0.2D, 0.2D, 0.15D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}, {0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D, 0.0D}};
 
     public double eval(Position paramPosition) {
-        int i = 0;
-        int j = 0;
-        if (6 == 6) {
-            i = 1;
+       // return Main.ce.eval(paramPosition);
+        if (this == Main.oe) {
+            return Main.ye.eval(paramPosition);
+        } else if (this == Main.ye) {
+            return Main.oe.eval(paramPosition);
         }
-        if (6 == 6) {
-            j = 1;
-        }
+        //throw new UnsupportedOperationException("lol");
 
-        double d = Math.random() - 0.5D;
-        for (int k = 0; k < paramPosition.board.length; k++) {
-            for (int m = 0; m < paramPosition.board[k].length; m++) {
-                if (paramPosition.board[k][m] != 0) {
-                    if (paramPosition.board[k][m] == 1) {
-                        d += 1000000000.0D;
-                    }
-                    if (paramPosition.board[k][m] == 2) {
-                        d += 9.0D;
-                    }
-                    if (paramPosition.board[k][m] == 3) {
-                        d += 5.25D;
-                    }
-                    if (paramPosition.board[k][m] == 4) {
-                        d += 3.25D;
-                    }
-                    if (paramPosition.board[k][m] == 5) {
-                        d += 3.0D;
-                    }
-                    if (paramPosition.board[k][m] == 6) {
-                        d += 1.0D;
-                    }
-                    if (paramPosition.board[k][m] == 7) {
-                        d -= 1000000000.0D;
-                    }
-                    if (paramPosition.board[k][m] == 8) {
-                        d -= 8.5D - 0.05D * m;
-                    }
-                    if (paramPosition.board[k][m] == 9) {
-                        d -= 4.75D - 0.05D * m;
-                    }
-                    if (paramPosition.board[k][m] == 10) {
-                        d -= 2.75D - 0.05D * m;
-                    }
-                    if (paramPosition.board[k][m] == 11) {
-                        d -= 2.5D - 0.05D * m;
-                    }
-                    if (paramPosition.board[k][m] == 12) {
-                        d -= 1.0D - 0.1D * m;
-                    }
+        return Main.ce.eval(paramPosition);
+//        System.out.println("My eval:");
+//        System.out.println("oe"+Main.oe);
+//        System.out.println("ye"+Main.ye);
+        //System.out.println(Main.oe);
+        //Main.oe = new MyNoobEvaluator();
+        //System.out.println(Main.oe);
+//////        int i = 0;
+//////        int j = 0;
+//////        if (6 == 6) {
+//////            i = 1;
+//////        }
+//////        if (6 == 6) {
+//////            j = 1;
+//////        }
+//////
+//////        double d = Math.random() - 0.5D;
+//////        for (int k = 0; k < paramPosition.board.length; k++) {
+//////            for (int m = 0; m < paramPosition.board[k].length; m++) {
+//////                if (paramPosition.board[k][m] != 0) {
+//////                    if (paramPosition.board[k][m] == 1) {
+//////                        d += 1000000000.0D;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 2) {
+//////                        d += 9.0D;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 3) {
+//////                        d += 5.25D;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 4) {
+//////                        d += 3.25D;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 5) {
+//////                        d += 3.0D;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 6) {
+//////                        d += 1.0D;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 7) {
+//////                        d -= 1000000000.0D;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 8) {
+//////                        d -= 8.5D - 0.05D * m;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 9) {
+//////                        d -= 4.75D - 0.05D * m;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 10) {
+//////                        d -= 2.75D - 0.05D * m;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 11) {
+//////                        d -= 2.5D - 0.05D * m;
+//////                    }
+//////                    if (paramPosition.board[k][m] == 12) {
+//////                        d -= 1.0D - 0.1D * m;
+//////                    }
+//////
+//////                    if (Position.isWhitePiece(paramPosition.board[k][m])) {
+//////                        d += whiteBoostTable[(m + j)][(k + i)];
+//////                    } else if (Position.isBlackPiece(paramPosition.board[k][m])) {
+//////                        d -= blackBoostTable[(m + j)][(k + i)];
+//////                    }
+//////
+//////                }
+//////
+//////            }
+//////
+//////        }
+//////
+//////        return d;
+    }
 
-                    if (Position.isWhitePiece(paramPosition.board[k][m])) {
-                        d += whiteBoostTable[(m + j)][(k + i)];
-                    } else if (Position.isBlackPiece(paramPosition.board[k][m])) {
-                        d -= blackBoostTable[(m + j)][(k + i)];
-                    }
-
-                }
-
-            }
-
-        }
-
-        return d;
+    @Override
+    public String toString() {
+        return "Jamo_my eval";
     }
 }
 
-class MyNoobEvaluator extends Evaluator{
+class MyNoobEvaluator extends Evaluator {
 
     @Override
     public double eval(Position p) {
         return 0d;
     }
-    
+
+    @Override
+    public String toString() {
+        return "lol";
+    }
 }
